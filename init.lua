a66f10073d48ac32dba545a0aac40c2168f301f9
clayzenx <clay.zenx@gmail.com>
2024-04-27 17:43:56 +0300

    require fix

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require 'lua.custom.config'
-require 'lua.custom.mapping'
+require 'custom.config'
+require 'custom.mapping'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
